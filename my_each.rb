b3de70956643f1368b2da8c05cfaf9dfb0c670fe
Cody Rickman <cody@creativemindstudios.com>
2018-11-13 17:56:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (collection) {|collection| puts collection}
+  if block_given?
+    yield
+  else
+    puts ""
+  end
+  collection
 end
